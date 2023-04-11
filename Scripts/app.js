91a850616b34f6d76f61a21e873f20363f6ed1af
Saba Sahar <saba.aafia@gmail.com>
2023-04-11 22:18:10 +0500

    task done

---
+++ Scripts/app.js
@@ -0,0 +1,92 @@
+
+    //       4th Aprail 
+    // var yourName = "Write your name here";
+    // alert(yourName);
+    
+    //       5th Aprail
+    // var yourNumber = 03210000000;
+    // console.log(yourNumber);
+
+    //       6th Aprail CONCATENATION
+//   var data1 = "Bano Qabil "
+//   var data2 = "Circle Foundation"
+//   var data3 = data1 + data2 //strings concatenation
+//   console.log(data3);
+
+// var data1 = "5"
+// var data2 = 5
+// var data3 = data1 + data2 //string and number concatenation
+// console.log(data3);
+
+// var data4 = data1.includes("5"); the result is true
+// console.log(data4);
+// console.log(+data1 + data2); whenever we add +sign or pareseInt() 0r Number  to string it becomes a number
+//    console.log(data1 - data2); result is 0
+
+//    var data1 = 10
+//    var data2 = 3
+//    console.log(data1 % data2); % tells us reminder
+//  var userName = prompt("What is your name?");
+// //  console.log(userName); //shows whatever user will write 
+//  document.write("welcome " + userName); the result is welcome saba
+
+//       //JS ZAKAT PERCENTAGE TASK
+//  var zakatPercentage = 0.025;
+//  var userInput = prompt("Write your amount here");
+//  var result = ( "your Zakat amount is " +userInput * zakatPercentage)
+//  alert(result)
+
+                  // 9th Aprail 2023
+    // ARTHEMATIC OPERATORS
+    // var a = 6;
+    // var b = 3;
+    //Increment
+    //    a++;
+      // document.write(a);// the result will be 7
+    //DECREMENT
+    // a--; the result will be 6
+    // // COMPOUND OPERATOR
+    // a += b;
+    // document.write(a); in real it is a= a+b, the result will be T
+
+
+    //COMPARISON OPERATORS
+    // var x=5
+    // var y=6
+    // console.log(x==y); it shows false
+    // console.log(x!=y); true
+
+    // var x=5
+    // var y="5"
+    // console.log(x===y); it will show false bcz === means check value as well as data type and here data type is not 
+    // console.log(x!==y)
+
+    //  var x=5;
+    //  var y=5;
+    // console.log(x>=y); result=true
+  
+             // 11th Aprail ******CONDITIONS****
+      // var x = 5
+      // var y = 6
+      // if(x!=y){document.write("this is true")}
+
+     //***** */ FITRA CALCULATOR* **********
+    //  var wheat = 170
+    //  var barley=560
+    //  var dates=1600
+    //  var raisin=2800
+    //  var userinput1 = prompt("Total number of family members:")
+    //  var userinput2 = prompt("Select your type:\n wheat \n barley \n dates \n raisin");
+    //  if (userinput2=="dates"){document.write("Your Fitra amount is: "  +userinput1*dates +  " PKR")}
+    //  if (userinput2=="wheat"){document.write("Your Fitra amount is: " +userinput1*wheat +  " PKR")}
+    //  if (userinput2=="barley"){document.write("Your Fitra amount is: " +userinput1*barley + " PKR")}
+    //  if (userinput2=="raisin"){document.write("Your Fitra amount is: " +userinput1*raisin + " PKR")}
+     
+         //******** LOGICAL OPERATORS********
+
+       
+
+     
+
+      
+      
